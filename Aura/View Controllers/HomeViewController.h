1ac35f01c7945b95417241b4e75ffb3451438fa8
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-07-22 18:28:49 -0400

    implemented log out functionality

---
+++ Aura/View Controllers/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Aura
+//
+//  Created by Storm Wright on 7/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Aura/View Controllers/HomeViewController.m
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.m
+//  Aura
+//
+//  Created by Storm Wright on 7/22/21.
+//
+
+#import "HomeViewController.h"
+#import "Parse/Parse.h"
+#import "AppDelegate.h"
+#import "LogInViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)logOut:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
+    
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LogInViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
