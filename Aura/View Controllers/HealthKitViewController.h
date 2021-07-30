3f91f1766cf28c3da8ed15bb76fed32a5be8a555
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-07-30 11:57:05 -0400

    created new view controller for HealthKit

---
+++ Aura/View Controllers/HealthKitViewController.h
@@ -0,0 +1,16 @@
+//
+//  HealthKitViewController.h
+//  Aura
+//
+//  Created by Storm Wright on 7/30/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HealthKitViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Aura/View Controllers/HealthKitViewController.m
@@ -0,0 +1,31 @@
+//
+//  HealthKitViewController.m
+//  Aura
+//
+//  Created by Storm Wright on 7/30/21.
+//
+
+#import "HealthKitViewController.h"
+
+@interface HealthKitViewController ()
+
+@end
+
+@implementation HealthKitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
