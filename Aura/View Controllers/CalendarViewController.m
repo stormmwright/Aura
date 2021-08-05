180e4ee3ff4366c541d812a409a1050cbdfeef3e
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-08-05 13:44:33 -0400

    successfully queried Parse for the proper color

--- Aura/View Controllers/CalendarViewController.m
+++ Aura/View Controllers/CalendarViewController.m
@@ -20,6 +20,8 @@
 
 @property (strong, nonatomic) NSArray *arrayOfEntries;
 
+@property (strong, nonatomic) NSMutableDictionary *dictOfEntries;
+
 @end
 
 @implementation CalendarViewController
@@ -30,8 +32,7 @@
     [self fetchEntries];
 }
 
-- (void)calendar:(FSCalendar *)calendar boundingRectWillChange:(CGRect)bounds animated:(BOOL)animated
-{
+- (void)calendar:(FSCalendar *)calendar boundingRectWillChange:(CGRect)bounds animated:(BOOL)animated {
     calendar.frame = (CGRect){calendar.frame.origin,bounds.size};
     // do other updates here
 }
@@ -44,7 +45,7 @@
     // fetch data asynchronously
     [query findObjectsInBackgroundWithBlock:^(NSArray *entries, NSError *error) {
         if (entries != nil) {
-            NSLog(@"%@", entries);
+//            NSLog(@"%@", entries);
             self.arrayOfEntries = entries;
         } else {
             NSLog(@"%@", error.localizedDescription);
@@ -53,19 +54,48 @@
 }
 
 // TODO: pair entry data with corresponding colors
-- (UIColor *)calendar:(FSCalendar *)calendar appearance:(FSCalendarAppearance *)appearance fillDefaultColorForDate:(NSDate *)date
-{
-    NSDictionary *fillDefaultColors = @{@"2021/08/01":[UIColor colorWithRed: 0.26 green: 0.63 blue: 0.28 alpha: 1.00], @"2021/08/02":[UIColor colorWithRed: 0.49 green: 0.70 blue: 0.26 alpha: 1.00],@"2021/08/03":[UIColor colorWithRed: 0.75 green: 0.79 blue: 0.20 alpha: 1.00], @"2021/08/04":[UIColor colorWithRed: 0.99 green: 0.85 blue: 0.21 alpha: 1.00], @"2021/08/05":[UIColor colorWithRed: 0.98 green: 0.55 blue: 0.00 alpha: 1.00], @"2021/08/06":[UIColor colorWithRed: 0.96 green: 0.32 blue: 0.12 alpha: 1.00], @"2021/08/07":[UIColor colorWithRed: 0.90 green: 0.22 blue: 0.21 alpha: 1.00]};
-    
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"yyyy/MM/dd"];
 
-    NSString *key = [formatter stringFromDate:date];
+- (UIColor *)getColor:(NSString *)mood {
+    NSDictionary *fillDefaultColors = @{@"Best":[UIColor colorWithRed: 0.26 green: 0.63 blue: 0.28 alpha: 1.00], @"Very Good":[UIColor colorWithRed: 0.49 green: 0.70 blue: 0.26 alpha: 1.00],@"Good":[UIColor colorWithRed: 0.75 green: 0.79 blue: 0.20 alpha: 1.00], @"Neutral":[UIColor colorWithRed: 0.99 green: 0.85 blue: 0.21 alpha: 1.00], @"Bad":[UIColor colorWithRed: 0.98 green: 0.55 blue: 0.00 alpha: 1.00], @"Very Bad":[UIColor colorWithRed: 0.96 green: 0.32 blue: 0.12 alpha: 1.00], @"Worst":[UIColor colorWithRed: 0.90 green: 0.22 blue: 0.21 alpha: 1.00]};
     
-    if ([fillDefaultColors.allKeys containsObject:key]) {
-        NSLog(@"%@", key);
-        return fillDefaultColors[key];
-    }
+    return fillDefaultColors[mood];
+}
+
+- (void)getMood:(NSDate *)date {
+    PFQuery *query = [PFQuery queryWithClassName:@"Entry"];
+    [query whereKey:@"author" equalTo:[PFUser currentUser]];
+    
+    NSDateComponents *day = [[NSDateComponents alloc] init];
+    day.day = 1;
+
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDate *nextDate = [calendar dateByAddingComponents:day toDate:date options:0];
+    
+    [query whereKey:@"createdAt" greaterThanOrEqualTo:date];
+    [query whereKey:@"createdAt" lessThan:nextDate];
+    query.limit = 1;
+    
+    NSLog(@"Date: @%@", date);
+    NSLog(@"Next Date: @%@", nextDate);
+    
+    __block Entry *entry = nil;
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *entries, NSError *error) {
+        if (entries != nil) {
+            NSLog(@"Mood query: %@", entries);
+            entry = entries.firstObject;
+            NSLog(@"Mood color: @%@", [self getColor:entry.mood]);
+            self.dictOfEntries[entry.createdAt] = entry;
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        
+        }
+    }];
+}
+
+- (UIColor *)calendar:(FSCalendar *)calendar appearance:(FSCalendarAppearance *)appearance fillDefaultColorForDate:(NSDate *)date {
+    [self getMood:date];
+    
     return nil;
 }
 
